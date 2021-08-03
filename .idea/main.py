a25436da4bdae0a3e3921bbeddb5cfeeb9490312
Cameron B <cameronbullis3500@gmail.com>
2021-08-03 17:24:52 -0400

    Completely ruining the application.

--- .idea/main.py
+++ .idea/main.py
@@ -1,28 +1,38 @@
 import time
 #Constants
 conjecturesTruth = True
+foreverLoop = True
 naughtyLoopNumbers = [1, -1, -5, -17]
 #Variables
 firstTime = True
 x = 0
+original = 0
 
-def conj_loop():
+def conj_loop(num):
     loop = True
-    x = int(input("Please enter an integer: "))
+
     while loop:
-        print(x)
-        if (x in naughtyLoopNumbers) and (firstTime == True):
+        print(num)
+        if (num in naughtyLoopNumbers) and (firstTime == True):
             print("You\'ll never prove the Collatz Conjecture that way. Let\'s try a different number.")
             loop = False
-        if (x in naughtyLoopNumbers) and (firstTime == False):
+
+        if (num in naughtyLoopNumbers) and (firstTime == False):
             print("You\'ll never prove the Collatz Conjecture that way... Try again...")
             loop = False
-        if (x % 2) == 0:
-            x = int(x / 2)
+        if (num % 2) == 0:
+            num = int(x / 2)
         else:
-            x = int(3 * x + 1)
+            num = int(3 * x + 1)
             time.sleep(.125)
 
-while conjecturesTruth:
-    conj_loop()
-    firstTime = False
+
+
+
+
+
+##while conjecturesTruth:
+##    x = int(input("Please enter an integer: "))
+##    original = x
+##    conj_loop(x)
+##    firstTime = False
