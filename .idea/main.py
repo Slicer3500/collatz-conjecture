2f3ec8bfe9f51b256d837647466afd0f4223c458
Cameron B <cameronbullis3500@gmail.com>
2021-08-04 11:16:23 -0400

    Fixed functionality, added brute force positive integer iterator. Need to fix firstTime bool.

--- .idea/main.py
+++ .idea/main.py
@@ -1,38 +1,53 @@
 import time
+
 #Constants
 conjecturesTruth = True
 foreverLoop = True
 naughtyLoopNumbers = [1, -1, -5, -17]
+
 #Variables
 firstTime = True
-x = 0
-original = 0
+iterationCount = 0
 
-def conj_loop(num):
+
+def iterator(num, iterationCount):
+    return num + iterationCount
+
+#def firstTimeFalse(firstTime):
+#    return firstTime = False
+
+def conjLoop(num):
     loop = True
-
     while loop:
         print(num)
+        if (num in naughtyLoopNumbers) and (firstTime == False):
+            print("You\'ll never prove the Collatz Conjecture that way... Trying another number...")
+            loop = False
+
         if (num in naughtyLoopNumbers) and (firstTime == True):
             print("You\'ll never prove the Collatz Conjecture that way. Let\'s try a different number.")
             loop = False
 
-        if (num in naughtyLoopNumbers) and (firstTime == False):
-            print("You\'ll never prove the Collatz Conjecture that way... Try again...")
-            loop = False
         if (num % 2) == 0:
-            num = int(x / 2)
+            num = int(num / 2)
+
         else:
-            num = int(3 * x + 1)
-            time.sleep(.125)
+            num = int(3 * num + 1)
+            time.sleep(.01)
 
+def iteratorPositiveBruteForceLoop(num):
+    while conjecturesTruth:
+        iterationCount = 0
+        conjLoop(num)
+        iterationCount += 1
+        num = iterator(num, iterationCount)
+        time.sleep(.5)
 
+def main():
+    num = int(input("Please enter an integer: "))
+    conjLoop(num)
+    #firstTime = False
+    num = int(input("Please enter an integer: "))
+    iteratorPositiveBruteForceLoop(num)
 
-
-
-
-##while conjecturesTruth:
-##    x = int(input("Please enter an integer: "))
-##    original = x
-##    conj_loop(x)
-##    firstTime = False
+main()
